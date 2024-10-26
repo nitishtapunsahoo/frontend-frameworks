9b19b0b526a3ad5e2b4c800c8759ab751d8e63c4
nitishtapunsahoo <nitishtapun.sahoo014152@rec.ac.in>
2024-10-26 21:17:07 +0530

    Create plants.js

---
+++ data/plants.js
@@ -0,0 +1,6 @@
+const plantsData = [
+  { id: 1, name: "Aloe Vera", price: 10, image: "/path-to-image/aloe-vera.jpg", inCart: false },
+  // Add five more plants with similar structure
+];
+
+export default plantsData;
