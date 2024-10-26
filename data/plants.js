4d09fdb57f3e14dce3f5b8e868a6afe7c901757a
nitishtapunsahoo <nitishtapun.sahoo014152@rec.ac.in>
2024-10-26 22:35:47 +0530

    Delete data directory

--- data/plants.js
+++
@@ -1,6 +0,0 @@
-const plantsData = [
-  { id: 1, name: "Aloe Vera", price: 10, image: "/path-to-image/aloe-vera.jpg", inCart: false },
-  // Add five more plants with similar structure
-];
-
-export default plantsData;
