1ee6291dc75da230939ffc4cb101b5643bd02dec
nitishtapunsahoo <nitishtapun.sahoo014152@rec.ac.in>
2024-10-26 21:11:49 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import LandingPage from './pages/LandingPage';
+import ProductListing from './pages/ProductListing';
+import CartPage from './pages/CartPage';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
